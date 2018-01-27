7d9bca58c1f06a2eaa7b0723a109bacb09c61776
apatron100 <30960286+apatron100@users.noreply.github.com>
2018-01-27 10:02:31 -0800

    first deploy

---
+++ server.js
@@ -0,0 +1,50 @@
+// Require our dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require ('path')
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require ('morgan')
+// Instantiate our Express App
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
+
+
+
+// Database
+require("./config/connection");
+
+// Use morgan logging
+app.use(logger("dev"));
+
+// BodyParser Settings
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Set up Handlebar for views
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Routes
+var routes = require('./controllers/news.js');
+app.use('/',routes);
+
+//404 Error
+app.use(function(req, res) {
+	res.render('404');
+});
+
+//Port
+app.listen(port, function() {
+    console.log("Listening on port:" + port);
+});
+
+ 
